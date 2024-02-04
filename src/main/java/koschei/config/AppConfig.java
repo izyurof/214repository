09f80aa1e9c37d0d4a0252a28c383cc9e0749168
AndryIzyurov <izyurof@gmail.com>
2024-02-04 19:08:40 +0300

    task solved

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,9 @@
 package koschei.config;
 
+import koschei.models.Death8;
+import koschei.models.Egg6;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,5 +16,13 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+    @Bean
+    public static Egg6 getEgg(Needle7 needle) {
+        return new Egg6(needle);
+    }
+    @Bean
+    public static Needle7 getNeedle(Death8 death) {
+        return new Needle7(death);
     }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,14 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+
 public class Needle7 {
+    private Death8 death;
+
+    public Needle7(Death8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
